38b2eb878fcf140796a340bdd64a10dcf7a3ee2a
Trezy <t@trezy.com>
2023-08-29 14:48:15 -0500

    stub the spawn system

---
+++ packages/spawn-system/src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Spawns new entities when appropriate.
+ *
+ * @param {object} store Statery store with the game's state.
+ */
+export function spawnSystem(store) {
+	console.log('spawnSystem', store)
+}
