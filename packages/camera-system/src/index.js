1ed7c9de89509688eb7fba7b88f908d8d8dd9e99
Trezy <t@trezy.com>
2023-08-29 14:48:06 -0500

    stub the camera system

---
+++ packages/camera-system/src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Updates the camera based on the player character's location.
+ *
+ * @param {object} store Statery store with the game's state.
+ */
+export function cameraSystem(store) {
+	console.log('cameraSystem', store)
+}
