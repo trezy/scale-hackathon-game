65b33384f02e9b35eba3cfbff9113dd1282a0260
Trezy <t@trezy.com>
2023-08-29 14:48:23 -0500

    stub the physics system

---
+++ packages/physics-system/src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Detects collisions and updates entities accordingly.
+ *
+ * @param {object} store Statery store with the game's state.
+ */
+export function physicsSystem(store) {
+	console.log('physicsSystem', store)
+}
