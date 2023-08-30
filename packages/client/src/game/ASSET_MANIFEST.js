c2a1734bbaa2e8f41a2d1074ad19aec4e2444746
Trezy <t@trezy.com>
2023-08-30 12:56:21 -0500

    add battlecruiser sprites

--- packages/client/src/game/ASSET_MANIFEST.js
+++ packages/client/src/game/ASSET_MANIFEST.js
@@ -7,6 +7,10 @@ export const ASSET_MANIFEST = {
 					name: 'fighter',
 					srcs: '/sprites/ships/team1/fighter.json',
 				},
+				{
+					name: 'battlecruiser',
+					srcs: '/sprites/ships/team1/battlecruiser.json',
+				},
 			],
 		},
 	],
