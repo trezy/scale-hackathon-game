981e523fe4d2d128e28b4e1a1af8987c76c2338d
Trezy <t@trezy.com>
2023-08-31 11:49:55 -0500

    cull projectiles once they're far off screen

--- packages/cull-system/src/cullSystem.js
+++ packages/cull-system/src/cullSystem.js
@@ -4,5 +4,19 @@
  * @param {object} store Statery store with the game's state.
  */
 export function cullSystem(store) {
-	// console.log('cullSystem', store)
+	store.set(state => {
+		const {
+			entities,
+			viewport,
+		} = state
+
+		return {
+			entities: entities.filter(entity => {
+				return entity.position.x > 0 - viewport.width
+					&& entity.position.x < viewport.width * 2
+					&& entity.position.y > 0 - viewport.height
+					&& entity.position.y < viewport.height * 2
+			}),
+		}
+	})
 }
