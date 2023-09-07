3adf583475397fe19261a1f6bcd201ab24691616
Trezy <t@trezy.com>
2023-09-07 01:58:20 -0500

    add basic entity typing

--- packages/store/src/store.js
+++ packages/store/src/store.js
@@ -1,4 +1,5 @@
 // Module imports
+import { CONTROL_BINDINGS } from '@space-game/static-data'
 import { makeStore } from 'statery'
 
 
@@ -6,8 +7,25 @@ import { makeStore } from 'statery'
 
 
 // Local imports
-import { CONTROL_BINDINGS } from '@space-game/static-data'
 import { generateInitialControlState } from './helpers/generateInitialControlState.js'
+
+
+
+
+
+// Types
+/**
+ * @typedef {object} Entity
+ * @property {object} [physics] Physics-related values.
+ * @property {import('@dimforge/rapier2d-compat').RigidBody} physics.body The entity's Rapier RigidBody.
+ * @property {import('@dimforge/rapier2d-compat').Collider} physics.collider The entity's Rapier Collider.
+ * @property {object} [position] The entity's current position vector.
+ * @property {number} position.x The entity's current position on the horizontal axis.
+ * @property {number} position.y The entity's current position on the vertical axis.
+ * @property {object} [velocity] The entity's current velocity vector.
+ * @property {number} velocity.x The entity's current velocity on the horizontal axis.
+ * @property {number} velocity.y The entity's current velocity on the vertical axis.
+ */
 
 
 
@@ -33,7 +51,7 @@ export const store = makeStore({
 
 	deltaMS: 0,
 
-	/** @type {object[]} */
+	/** @type {Entity[]} */
 	entities: [],
 
 	/** @type {number[]} */
